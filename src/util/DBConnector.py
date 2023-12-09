b3497bb1c0a88834975f5b3c1da12a7d49fc3b22
girishbhatta93@gmail.com <girishb@humanforce.com>
2023-12-09 23:57:56 +1100

    added singelon db connector class

---
+++ src/util/DBConnector.py
@@ -0,0 +1,18 @@
+from util import Singleton
+import psycopg2
+
+
+@Singleton
+class DBConnector:
+    def __init__(self, db_provider, username, password, host, port, db):
+        self._db_provider = db_provider
+        self._username = username
+        self._password = password
+        self._host = host
+        self._port = port
+        self._db = db
+
+    def get_conn(self):
+        connection_str: str = f"{self._db_provider}://{self._username}:{self._password}@{self._host}/{self._db}"
+        with psycopg2.connect(connection_str) as conn:
+            return conn
