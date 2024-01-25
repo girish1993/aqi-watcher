694c7a3cfa1c5ef7b3886e710e2c98af1cc98989
girishbhatta93@gmail.com <girishb@humanforce.com>
2024-01-25 18:22:01 +1100

    Formatted results for measurements as well

--- app/include/dto/response_formulator.py
+++ app/include/dto/response_formulator.py
@@ -29,10 +29,25 @@ class Parameters:
     location_id: int = None
 
 
+@dataclass
+class Measurements:
+    locationId: int
+    location: str
+    parameter: str
+    value: float
+    date: Dict[str, str]
+    unit: str
+    coordinates: Dict[str, float]
+    country: str
+    city: Optional[str]
+    entity: Optional[str]
+
+
 class DataFactory:
     _CLASS_MAPPING: dict = {
         "locations": Locations,
-        "parameters": Parameters
+        "parameters": Parameters,
+        "measurements": Measurements
     }
     _results = {}
 
@@ -51,9 +66,10 @@ class DataFactory:
                     parameter = cls._CLASS_MAPPING.get("parameters")(**param_item)
                     parameter.location_id = item.get("id")
                     list_of_associated_params.append(parameter.__dict__)
-
             cls._results[key] = list_of_locations
             cls._results["parameters"] = list_of_associated_params
+        else:
+            cls._results[key] = [cls_type(**item).__dict__ for item in data]
 
     @classmethod
     def formulate(cls, data: List[List[Dict]]) -> Dict[str, List[Dict]]:
@@ -63,4 +79,7 @@ class DataFactory:
                 data = res_obj.get(key).get("results")
                 print("key", key)
                 cls.create_instance_and_populate(key=key, data=data)
+
+        results = [{k: len(v)} for k, v in cls._results.items()]
+        print("results before returning", results)
         return cls._results
